4516ec0f54a84c7bc79546e0ae94869595e12464
Mikael Andersson <mik4el.4ndersson@gmail.com>
2015-02-07 22:10:54 +0100

    added first slate to build on

---
+++ next_bus.py
@@ -0,0 +1,12 @@
+def minutes_to_next_bus():
+    minutes = 0
+    return minutes
+
+
+def print_next_bus():
+    print "next bus leaves in: %s" % (minutes_to_next_bus())
+    return
+
+
+if __name__ == "__main__":
+    print_next_bus()
---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+from next_bus import minutes_to_next_bus
+
+
+class TestMinutesToNextBus(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_minutes_to_next_bus(self):
+        result = minutes_to_next_bus()
+        self.assertEqual(result, 0)
