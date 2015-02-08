ed4095a57661a282b1b81832aa5e72f25e26542c
Mikael Andersson <mik4el.4ndersson@gmail.com>
2015-02-08 17:35:20 +0100

    OOP and exception handling

--- next_bus.py
+++ next_bus.py
@@ -2,86 +2,100 @@ from config import credentials
 import requests
 
 
-def minutes_to_next_bus():
-    """
-    Gets minutes to next bus from Trafiklab Api
+class NextBusChecker:
+    def get_data_from_api(self):
+        """
+        Gets data from Trafiklab Api
 
-    Api call format:
-    https://api.sl.se/api2/realtimedepartures.json?key=<TRAFIKLAB_API_KEY>&siteid=<SITEID>&timewindow=<TIMEWINDOW>
-    SITEID for Ekensberg is 1600 (2015-02-07)
-    TIMEWINDOW is integer for minutes from now, max is 60.
+        Api call format:
+        https://api.sl.se/api2/realtimedepartures.json?key=<TRAFIKLAB_API_KEY>&siteid=<SITEID>&timewindow=<TIMEWINDOW>
+        SITEID for Ekensberg is 1600 (2015-02-07)
+        TIMEWINDOW is integer for minutes from now, max is 60.
 
-    Api call for Ekensberg for 60 minutes:
-    https://api.sl.se/api2/realtimedepartures.json?key=<TRAFIKLAB_API_KEY>&siteid=1600&timewindow=60
+        Api call for Ekensberg for 60 minutes:
+        https://api.sl.se/api2/realtimedepartures.json?key=<TRAFIKLAB_API_KEY>&siteid=1600&timewindow=60
 
-    Example response:
-    {
-        "StatusCode": 0,
-        "Message": null,
-        "ExecutionTime": 50,
-        "ResponseData": {
-            "LatestUpdate": "2015-02-07T22:35:44",
-            "DataAge": 15,
-            "Metros": [],
-            "Buses": [
-                {
-                    "JourneyDirection": 2,
-                    "GroupOfLine": null,
-                    "StopAreaName": "Ekensberg",
-                    "StopAreaNumber": 14066,
-                    "StopPointNumber": 14066,
-                    "StopPointDesignation": null,
-                    "TimeTabledDateTime": "2015-02-07T23:00:00",
-                    "ExpectedDateTime": "2015-02-07T23:00:00",
-                    "DisplayTime": "24 min",
-                    "Deviations": null,
-                    "TransportMode": "BUS",
-                    "LineNumber": "133",
-                    "Destination": "Liljeholmen",
-                    "SiteId": 1600
-                },
-                {
-                    "JourneyDirection": 2,
-                    "GroupOfLine": null,
-                    "StopAreaName": "Ekensberg",
-                    "StopAreaNumber": 14066,
-                    "StopPointNumber": 14066,
-                    "StopPointDesignation": null,
-                    "TimeTabledDateTime": "2015-02-07T23:30:00",
-                    "ExpectedDateTime": "2015-02-07T23:30:00",
-                    "DisplayTime": "23:30",
-                    "Deviations": null,
-                    "TransportMode": "BUS",
-                    "LineNumber": "133",
-                    "Destination": "Liljeholmen",
-                    "SiteId": 1600
-                }
-            ],
-            "Trains": [],
-            "Trams": [],
-            "Ships": [],
-            "StopPointDeviations": []
+        Example response:
+        {
+            "StatusCode": 0,
+            "Message": null,
+            "ExecutionTime": 50,
+            "ResponseData": {
+                "LatestUpdate": "2015-02-07T22:35:44",
+                "DataAge": 15,
+                "Metros": [],
+                "Buses": [
+                    {
+                        "JourneyDirection": 2,
+                        "GroupOfLine": null,
+                        "StopAreaName": "Ekensberg",
+                        "StopAreaNumber": 14066,
+                        "StopPointNumber": 14066,
+                        "StopPointDesignation": null,
+                        "TimeTabledDateTime": "2015-02-07T23:00:00",
+                        "ExpectedDateTime": "2015-02-07T23:00:00",
+                        "DisplayTime": "24 min",
+                        "Deviations": null,
+                        "TransportMode": "BUS",
+                        "LineNumber": "133",
+                        "Destination": "Liljeholmen",
+                        "SiteId": 1600
+                    },
+                    {
+                        "JourneyDirection": 2,
+                        "GroupOfLine": null,
+                        "StopAreaName": "Ekensberg",
+                        "StopAreaNumber": 14066,
+                        "StopPointNumber": 14066,
+                        "StopPointDesignation": null,
+                        "TimeTabledDateTime": "2015-02-07T23:30:00",
+                        "ExpectedDateTime": "2015-02-07T23:30:00",
+                        "DisplayTime": "23:30",
+                        "Deviations": null,
+                        "TransportMode": "BUS",
+                        "LineNumber": "133",
+                        "Destination": "Liljeholmen",
+                        "SiteId": 1600
+                    }
+                ],
+                "Trains": [],
+                "Trams": [],
+                "Ships": [],
+                "StopPointDeviations": []
+            }
         }
-    }
-    """
+        """
 
-    api_url = "https://api.sl.se/api2/realtimedepartures.json?key=%s&siteid=1600&timewindow=60" % credentials.TRAFIKLAB_API_KEY
-    #print "Accessing api using url: %s" % api_url
+        api_url = "https://api.sl.se/api2/realtimedepartures.json?key=%s&siteid=1600&timewindow=60" % credentials.TRAFIKLAB_API_KEY
+        #print "Accessing api using url: %s" % api_url
 
-    api_request = requests.get(api_url)
+        try:
+            api_request = requests.get(api_url, timeout=10)
+        except:
+            api_result = "No data"
+        else:
+            api_result = api_request.json()
+        #print "Result: %s" % api_result
 
-    api_result = api_request.json()
-    #print "Result: %s" % api_result
+        return api_result
 
-    minutes = api_result["ResponseData"]["Buses"][0]["DisplayTime"]  # Very unsafe
+    def minutes_to_next_bus(self):
+        """
+        Gets minutes to next bus
+        """
+        api_result = self.get_data_from_api()
 
-    return minutes
+        try:
+            minutes = api_result["ResponseData"]["Buses"][0]["DisplayTime"]
+        except (KeyError, TypeError):
+            minutes = "No data"
+        return minutes
 
-
-def print_next_bus():
-    print "next bus leaves in: %s" % minutes_to_next_bus()
-    return
+    def print_next_bus(self):
+        print "Next bus leaves in: %s" % self.minutes_to_next_bus()
+        return
 
 
 if __name__ == "__main__":
-    print_next_bus()
+    next_bus_checker = NextBusChecker()
+    next_bus_checker.print_next_bus()
--- tests.py
+++ tests.py
@@ -1,11 +1,68 @@
 import unittest
-from next_bus import minutes_to_next_bus
+from next_bus import NextBusChecker
+from mock import Mock
 
 
 class TestMinutesToNextBus(unittest.TestCase):
     def setUp(self):
-        pass
+        self.next_bus_checker = NextBusChecker()
 
-    def test_minutes_to_next_bus(self):
-        result = minutes_to_next_bus()
-        self.assertNotEqual(result, 0)
+    def test_minutes_to_next_bus_standard_api_call(self):
+        mock_get_data_from_api_return_value = {u"ExecutionTime": 92,
+                                               u'ResponseData': {u'LatestUpdate': u'2015-02-08T17:10:50', u'Buses': [
+                                                   {u'TimeTabledDateTime': u'2015-02-08T17:15:00',
+                                                    u'Destination': u'Liljeholmen', u'DisplayTime': u'3 min',
+                                                    u'StopPointNumber': 14066,
+                                                    u'ExpectedDateTime': u'2015-02-08T17:15:00',
+                                                    u'TransportMode': u'BUS', u'Deviations': None, u'GroupOfLine': None,
+                                                    u'StopAreaName': u'Ekensberg', u'SiteId': 1600,
+                                                    u'LineNumber': u'133', u'StopAreaNumber': 14066,
+                                                    u'StopPointDesignation': None, u'JourneyDirection': 2},
+                                                   {u'TimeTabledDateTime': u'2015-02-08T17:30:00',
+                                                    u'Destination': u'Liljeholmen', u'DisplayTime': u'18 min',
+                                                    u'StopPointNumber': 14066,
+                                                    u'ExpectedDateTime': u'2015-02-08T17:30:00',
+                                                    u'TransportMode': u'BUS', u'Deviations': None, u'GroupOfLine': None,
+                                                    u'StopAreaName': u'Ekensberg', u'SiteId': 1600,
+                                                    u'LineNumber': u'133', u'StopAreaNumber': 14066,
+                                                    u'StopPointDesignation': None, u'JourneyDirection': 2},
+                                                   {u'TimeTabledDateTime': u'2015-02-08T17:45:00',
+                                                    u'Destination': u'Liljeholmen', u'DisplayTime': u'17:45',
+                                                    u'StopPointNumber': 14066,
+                                                    u'ExpectedDateTime': u'2015-02-08T17:45:00',
+                                                    u'TransportMode': u'BUS', u'Deviations': None, u'GroupOfLine': None,
+                                                    u'StopAreaName': u'Ekensberg', u'SiteId': 1600,
+                                                    u'LineNumber': u'133', u'StopAreaNumber': 14066,
+                                                    u'StopPointDesignation': None, u'JourneyDirection': 2},
+                                                   {u'TimeTabledDateTime': u'2015-02-08T18:00:00',
+                                                    u'Destination': u'Liljeholmen', u'DisplayTime': u'18:00',
+                                                    u'StopPointNumber': 14066,
+                                                    u'ExpectedDateTime': u'2015-02-08T18:00:00',
+                                                    u'TransportMode': u'BUS', u'Deviations': None, u'GroupOfLine': None,
+                                                    u'StopAreaName': u'Ekensberg', u'SiteId': 1600,
+                                                    u'LineNumber': u'133', u'StopAreaNumber': 14066,
+                                                    u'StopPointDesignation': None, u'JourneyDirection': 2}],
+                                                                 u'Ships': [], u'StopPointDeviations': [], u'Trams': [],
+                                                                 u'DataAge': 13, u'Trains': [], u'Metros': []},
+                                               u'Message': None, u'StatusCode': 0}
+        self.next_bus_checker.get_data_from_api = Mock(return_value=mock_get_data_from_api_return_value)
+        result = self.next_bus_checker.minutes_to_next_bus()
+        self.assertEqual(result, "3 min")
+
+    def test_minutes_to_next_bus_error_in_api_result(self):
+        mock_get_data_from_api_return_value = {}
+        self.next_bus_checker.get_data_from_api = Mock(return_value=mock_get_data_from_api_return_value)
+        result = self.next_bus_checker.minutes_to_next_bus()
+        self.assertEqual(result, "No data")
+        mock_get_data_from_api_return_value = []
+        self.next_bus_checker.get_data_from_api = Mock(return_value=mock_get_data_from_api_return_value)
+        result = self.next_bus_checker.minutes_to_next_bus()
+        self.assertEqual(result, "No data")
+        mock_get_data_from_api_return_value = ""
+        self.next_bus_checker.get_data_from_api = Mock(return_value=mock_get_data_from_api_return_value)
+        result = self.next_bus_checker.minutes_to_next_bus()
+        self.assertEqual(result, "No data")
+        mock_get_data_from_api_return_value = "No data"
+        self.next_bus_checker.get_data_from_api = Mock(return_value=mock_get_data_from_api_return_value)
+        result = self.next_bus_checker.minutes_to_next_bus()
+        self.assertEqual(result, "No data")
